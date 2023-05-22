949748a34d5e3e383d206b4ea558a8849a1569ba
dario_ceni <dario01ceni@gmail.com>
2023-05-22 15:49:40 +0200

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import time
+from datetime import datetime, timedelta
 
 
 # df = pd.DataFrame(
@@ -17,9 +19,21 @@ tweets_by_week = data.resample('W').apply(list)
 
 df_list = []
 
-for week_start, week_analysis in tweets_by_week.iterrows():
+for end_week_day, week_analysis in tweets_by_week.iterrows():
     current_week = []
-    current_week.append(week_start.strftime('%Y-%m-%d'))
+
+    # tutte ste righe per far sì che la label si "12-19" dove 12 è il primo giorno della settimana in esame e 19 è l'ultimo
+    endDay_week_string = end_week_day.strftime('%Y-%m-%d')
+    date = datetime.strptime(endDay_week_string, '%Y-%m-%d')
+    week_before = date - timedelta(weeks=1)
+    firstDay_week_string = week_before.strftime('%Y-%m-%d')
+    endDay_week_string = endDay_week_string[-2:]
+    firstDay_week_string = firstDay_week_string[-2:]
+    week_label = firstDay_week_string + "-" + endDay_week_string
+
+    current_week.append(week_label)
+
+
     vader_negative = week_analysis['vader_SCORE_pnn_numeric'].count(-1.0)
     current_week.append(vader_negative)
 
@@ -29,7 +43,6 @@ for week_start, week_analysis in tweets_by_week.iterrows():
     vader_positive = week_analysis['vader_SCORE_pnn_numeric'].count(1.0)
     current_week.append(vader_positive)
     df_list.append(current_week)
-    # print(current_week)
 
     df = pd.DataFrame(df_list, columns=["weeks", "negative", "neutral", "positive"])
 
