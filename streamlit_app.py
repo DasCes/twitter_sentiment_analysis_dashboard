c6cec56b879640a183973079c06bf17e268eb9ee
dario_ceni <dario01ceni@gmail.com>
2023-05-23 16:55:22 +0200

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,10 @@ data['created_at'] = pd.to_datetime(data['created_at'])
 end_date = data['created_at'].max().date()  # Get the maximum date in the 'created_at' column
 start_date = end_date - timedelta(days=30)  # Subtract 30 days from the end date
 lastMonth_data = data[(data['created_at'].dt.date >= start_date) & (data['created_at'].dt.date <= end_date)]
-data.set_index("created_at", inplace=True)
+lastMonth_data.set_index("created_at", inplace=True)
 lastMonth_data = lastMonth_data.resample('D').apply(list)
 
 
-data['created_at'] = pd.to_datetime(data['created_at'])
-data.set_index("created_at", inplace=True)
 tweets_by_week = data.resample('W').apply(list)
 
 
