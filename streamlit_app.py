5b8cea480a02b8ee7282c1228b42a5d788bac893
dario_ceni <dario01ceni@gmail.com>
2023-05-23 16:09:17 +0200

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,11 @@ data = pd.read_csv('data/all_at_21_05_2023.csv', index_col=[0])
 data['created_at'] = pd.to_datetime(data['created_at'])
 data.set_index("created_at", inplace=True)
 tweets_by_week = data.resample('W').apply(list)
-tweets_by_month = data.resample('M').apply(list)
+
+end_date = data['created_at'].max().date()  # Get the maximum date in the 'created_at' column
+start_date = end_date - timedelta(days=30)  # Subtract 30 days from the end date
+lastMonth_data = data.loc[(data['created_at'].dt.date >= start_date) & (data['created_at'].dt.date <= end_date), ['text']]
+
 df_list = []
 custom_labels = []
 
@@ -132,39 +136,28 @@ def main():
             if data_size == "last month":
                 st.title("Sentiment analysis on last month dataset", anchor=None, help=None)
                 df_list = []
-                custom_labels = []
-                for end_week_day, week_analysis in tweets_by_month.iterrows():
-                    current_week = []
-                    # tutte ste righe per far sì che la label si "12-19" dove 12 è il primo giorno della settimana in esame e 19 è l'ultimo
-                    endDay_week_string = end_week_day.strftime('%Y-%m-%d')
-                    date = datetime.strptime(endDay_week_string, '%Y-%m-%d')
-                    week_before = date - timedelta(weeks=1)
-                    firstDay_week_string = week_before.strftime('%Y-%m-%d')
-                    endDay_week_string = endDay_week_string[-2:]
-                    firstDay_week_string = firstDay_week_string[-2:]
-                    month = week_before.strftime('%b')
+                current_month = []
+                for day, day_analysis in lastMonth_data.iterrows():
+                    current_day = []
 
-                    week_label = month + " " + firstDay_week_string + "-" + endDay_week_string
+                    current_day.append(day)
 
-                    print(week_label)
-                    custom_labels.append(week_label)
-                    current_week.append(end_week_day)
+                    vader_negative = day_analysis['xlm_roberta_SCORE_numeric'].count(-1.0)
+                    current_day.append(vader_negative)
 
-                    vader_negative = week_analysis['xlm_roberta_SCORE_numeric'].count(-1.0)
-                    current_week.append(vader_negative)
+                    vader_neutral = day_analysis['xlm_roberta_SCORE_numeric'].count(0.0)
+                    current_day.append(vader_neutral)
 
-                    vader_neutral = week_analysis['xlm_roberta_SCORE_numeric'].count(0.0)
-                    current_week.append(vader_neutral)
-
-                    vader_positive = week_analysis['xlm_roberta_SCORE_numeric'].count(1.0)
-                    current_week.append(vader_positive)
-                    df_list.append(current_week)
+                    vader_positive = day_analysis['xlm_roberta_SCORE_numeric'].count(1.0)
+                    current_day.append(vader_positive)
+                    df_list.append(current_day)
 
                 df = pd.DataFrame(df_list, columns=["weeks", "negative", "neutral", "positive"])
                 st.subheader('xlm sentiment analysis')
                 fig = px.bar(df, x="weeks", y=["negative", "neutral", "positive"], barmode='group', height=400)
-                fig.update_xaxes(ticktext=custom_labels, tickvals=df['weeks'])
+                # fig.update_xaxes(ticktext=custom_labels, tickvals=df['weeks'])
                 st.plotly_chart(fig)
+
 
             if data_size == "last week":
                 st.title("Sentiment analysis on last week dataset", anchor=None, help=None)
